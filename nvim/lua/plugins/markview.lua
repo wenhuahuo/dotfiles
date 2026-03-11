22f19f53e5061fbe5063311a8c1fd6216b6e1c18
Wenhua Huo <wenhua_huo@163.com>
2026-03-11 19:58:02 +0800

    Add: latex support in markview

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -5,4 +5,19 @@ return {
 
     -- Completion for `blink.cmp`
     -- dependencies = { "saghen/blink.cmp" },
+	config = function()
+		require("markview").setup({
+			latex = {
+				enable = true,     -- 启用 LaTeX 支持
+				-- 内联渲染
+				inline = { enable = true },
+				-- 块级公式渲染
+				block  = { enable = true },
+				-- 括号/符号等可选
+				brackets = { enable = true, hl = "@punctuation.brackets" },
+		  	},
+		  	-- （可选）Markdown 预览配置
+		  	markdown = { enable = true },
+		})
+	  end,
 };
